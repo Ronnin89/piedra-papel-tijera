e4bd5fb6596743ad6d5b77efacb00e422104dbf0
Ricardo Alvarez <ri.alvarez.89@icloud.com>
2021-01-05 22:11:05 -0300

    programa terminado

--- juego.rb
+++ juego.rb
@@ -3,8 +3,38 @@ choice = ARGV[0]
 choice_pc = rand(3)
 
 unless choice == 'piedra' || choice == 'papel' || choice == 'tijeras'
-    puts 'Argumento invalido: Debe ser piedra, papel o tijera'
+    puts 'Argumento invalido: Debe ser piedra, papel o tijeras'
     exit()
 end
 
-puts choice_pc
+choice_pc = 'piedra' if choice_pc == 0
+choice_pc = 'papel' if choice_pc == 1
+choice_pc = 'tijeras' if choice_pc == 2
+
+puts "Computador juega #{choice_pc}"
+
+if choice == 'tijeras'
+    if choice_pc == 'piedra'
+        puts 'Perdiste'
+    elsif choice_pc == 'papel'
+        puts 'Ganaste'        
+    else
+        puts 'Empataste'
+    end
+elsif choice == 'papel'
+    if choice_pc == 'piedra'
+        puts 'Ganaste'
+    elsif choice_pc == 'papel'
+        puts 'Empataste'        
+    else
+        puts 'Perdiste'
+    end
+else
+    if choice_pc == 'piedra'
+        puts 'Empataste'
+    elsif choice_pc == 'papel'
+        puts 'Perdiste'        
+    else
+        puts 'Ganaste'
+    end
+end
